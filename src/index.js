a48caec83db8c82081b98b8a4c8cbf830e253fdf
nantenaina <nantenaina@qdata-mg.com>
2022-05-11 17:54:47 +0300

    Add dependance with interface

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+//for version 18 >
+// import {StrictMode} from 'react';
+// import {createRoot} from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaListAlt, FaCheckSquare, FaPlusSquare,FaTrash} from 'react-icons/fa';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+class App extends React.Component{
+    render(){
+        return (
+            <section id="todo">
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Ranger la vaisselle 
+                        <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Répondre appel d'offres
+                        <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Signer contrat
+                        <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Ranger la salon
+                        <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                </ul>
+                <footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
+                    <div className="btn-group">
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaListAlt /></a>
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare /></a>
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare /></a>
+                    </div>
+                    <button className="btn btn-outline-dark bg-light"><FaTrash /></button>
+                </footer>
+            </section>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>,
+//   );
